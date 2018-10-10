2a5c6e8134bd7f1993cf6175a4a13f69fbef8437
Sung J Byun <sungjbyun@D-10-19-184-28.dhcp4.washington.edu>
2018-10-09 19:12:09 -0700

    update with doc

--- HW03/src/com/example/java/Main.java
+++ HW03/src/com/example/java/Main.java
@@ -1,24 +1,37 @@
+/*
+CSC 142
+Justin Byun No partner at this time
+HW#03 Last updated 10/09
+appx. 4hours elapsed
+Used IDE: IntelliJ(Eclipse)
+*/
+
 package com.example.java;
-import java.lang.Math;
 import java.util.*;
 
 public class Main {
-
     public static void main(String[] args) {
-        /* prompt message to the end user to start off and take in the user inputs
-         * subsequently, pass the input value to the method area
-         *  And report results in the end
-         */
-        System.out.println("Enter the radius and height to calculate the surface area of a cylinder: ");
+    /* prompt message to the end user to start off and take in the user inputs
+    * subsequently, pass the input value to the method area
+    *  And report results in the end
+    */
+        System.out.print("Enter the string that you wanna FLIP!");
         Scanner console = new Scanner(System.in);
-        double radius = console.nextDouble();
-        double height = console.nextDouble();
-
-        System.out.println("Surface area of a cylinder is "+ cylinderSurfaceArea(radius, height));
+        String input_str = console.nextLine();
+        printReverse(input_str);
     }
-    public static double cylinderSurfaceArea(double r, double h) {
-        // execute the proper math function after importing java.Math
-        double area = 2*Math.PI*r*(r + h);
-        return area;
+    public static void printReverse(String str) {
+        // print the reversed string
+        // in case it entered value is blank,
+        // print a blank: if its not blank,
+        // iterate backward and print the character
+        int i = str.length()-1;
+        if (str.length() == 0) {
+            System.out.println(" ");
+        } else {
+            for (; i >= 0; i--) {
+                System.out.print(str.charAt(i));
+            }
+        }
     }
 }
