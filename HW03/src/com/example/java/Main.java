e9a674648f862b1b4c9526102f6c11659e87d3bd
Sung J Byun <sungjbyun@D-10-18-213-71.dhcp4.washington.edu>
2018-10-11 18:31:24 -0700

    tidy-ing up

--- HW03/src/com/example/java/Main.java
+++ HW03/src/com/example/java/Main.java
@@ -1,37 +1,19 @@
-/*
-CSC 142
-Justin Byun No partner at this time
-HW#03 Last updated 10/09
-appx. 4hours elapsed
-Used IDE: IntelliJ(Eclipse)
-*/
-
 package com.example.java;
 import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
-    /* prompt message to the end user to start off and take in the user inputs
-    * subsequently, pass the input value to the method area
-    *  And report results in the end
-    */
         System.out.print("Enter the string that you wanna FLIP!");
         Scanner console = new Scanner(System.in);
-        String input_str = console.nextLine();
-        printReverse(input_str);
+        String inputs = console.nextLine();
+        System.out.println(print_reverse(inputs));
     }
-    public static void printReverse(String str) {
-        // print the reversed string
-        // in case it entered value is blank,
-        // print a blank: if its not blank,
-        // iterate backward and print the character
-        int i = str.length()-1;
-        if (str.length() == 0) {
-            System.out.println(" ");
-        } else {
-            for (; i >= 0; i--) {
-                System.out.print(str.charAt(i));
-            }
+    public static String print_reverse(String str) {
+        int i = (str.length() -1);
+        String word = "";
+        for (; i>=0; i--) {
+            word += str.charAt(i);
         }
+        return word;
     }
 }
