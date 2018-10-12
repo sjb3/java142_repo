4cd29376c9c2e37a4e2db6d79ec0d9708499029d
Sung J Byun <sungjbyun@D-10-18-213-71.dhcp4.washington.edu>
2018-10-11 19:30:22 -0700

    review-ing

--- HW02_extra/makeV.java
+++ HW02_extra/makeV.java
@@ -9,13 +9,18 @@
 public class makeV {
   public static void main(String[] args) {
     for(int i=1; i<=3; i++) {
-      for(int j=1; j<= i-3; j++) {
+
+      for(int j=1; j <=i; j++) {
+        System.out.print("$");
+      }
+           for(int j=0; j<= 3-i; j++) {
         System.out.print("*");
       }
-      for(int j=1; j <=3-2; j++) {
-        System.out.print("$");
+             for(int j=0; j<= 3-i; j++) {
+        System.out.print("*");
       }
       System.out.println();
     }
+// in progress
   }
 }
--- HW03/src/com/example/java/Main.java
+++ HW03/src/com/example/java/Main.java
@@ -3,17 +3,16 @@ import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.print("Enter the string that you wanna FLIP!");
-        Scanner console = new Scanner(System.in);
-        String inputs = console.nextLine();
-        System.out.println(print_reverse(inputs));
-    }
-    public static String print_reverse(String str) {
-        int i = (str.length() -1);
-        String word = "";
-        for (; i>=0; i--) {
-            word += str.charAt(i);
+
+        for(int i=1; i<=5 ; i++){
+            for (int j=1; j<=5-i; j++) {
+                System.out.print("*");
+            }
+            for(int j=1; j<=i; j++) {
+                System.out.print(i);
+            }
+            System.out.println();
         }
-        return word;
+
     }
 }
