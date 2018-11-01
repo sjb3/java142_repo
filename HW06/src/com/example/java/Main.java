79d542c93011e53dda32d2c5b96b8e0f5138b2dc
Sung J Byun <sungjbyun@Sungs-MacBook-Pro-2.local>
2018-11-01 15:10:59 -0700

    working on 2nd exam

--- HW06/src/com/example/java/Main.java
+++ HW06/src/com/example/java/Main.java
@@ -1,36 +1,26 @@
 package com.example.java;
 import java.util.*;
 import java.io.*;
+import java.lang.Math;
 
 public class Main {
 
     public static void main(String[] args)
             throws FileNotFoundException {
-        Scanner data = new Scanner(new File("filename"));
-        boyGirl(data);
+        Scanner input = new Scanner(new File("input.txt"));
+        apples(input);
     }
 
-    public static void boyGirl(Scanner data) {
-//        Scanner data = new Scanner(input);
-        int line_num = 0;
-        while (data.hasNextLine()) {
-            line_num++;
-            int boy_count = 0;
-            int girl_count = 0;
-            int boy_sum = 0;
-            int girl_sum = 0;
-            if (line_num % 2 != 0) {
-                boy_count++;
-                boy_sum += data.nextInt();
-            } else {
-                girl_count++;
-                girl_sum += data.nextInt();
-            }
-            System.out.println("boy_count" + boy_count);
-            System.out.println("girl_count" + girl_count);
-            System.out.println("boy_sum" + boy_sum);
-            System.out.println("girl_sum" + girl_sum);
+    public static void apples(Scanner input) {
 
-        }
+            while (input.hasNextLine()) {
+                int id = input.nextInt();
+                String f_name = input.next();
+                String l_name = input.next();
+
+                System.out.printf("%d %s %s\n", id, f_name, l_name);
+            }
+
+
     }
 }
--- _2nd_exam/boyGirl.java
+++ _2nd_exam/boyGirl.java
@@ -1,35 +1,47 @@
 package com.example.java;
 import java.util.*;
 import java.io.*;
+import java.lang.Math;
 
 public class Main {
 
     public static void main(String[] args)
             throws FileNotFoundException {
-        Scanner data = new Scanner(new File("filename"));
-        boyGirl(data);
+        Scanner input = new Scanner(new File("input.txt"));
+        boyGirl(input);
     }
 
-    public static void boyGirl(Scanner data) {
-//        Scanner data = new Scanner(input);
-        int line_num = 0;
-        while (data.hasNextLine()) {
-            line_num++;
-            int boy_count = 0;
-            int girl_count = 0;
-            int boy_sum = 0;
-            int girl_sum = 0;
-            if (line_num % 2 != 0) {
-                boy_count++;
+    public static void boyGirl(Scanner input) {
+        int line_Num = 0;
+        int boy_count = 0;
+        int girl_count = 0;
+        int boy_sum = 0;
+        int girl_sum = 0;
+
+        while(input.hasNextLine()) {
+            String text = input.nextLine();
+            line_Num++;
+
+            Scanner data = new Scanner(text);
+            String name = data.next();
+//            System.out.println(name);
+//            System.out.println(val);
+//            System.out.println(line_Num);
+//            int val = data.nextInt();
+
+            if(line_Num%2 !=0) {
+                boy_count ++;
                 boy_sum += data.nextInt();
+//                System.out.println("LINE_NUM: " + line_Num);
+
             } else {
-                girl_count++;
+                girl_count ++;
                 girl_sum += data.nextInt();
+//                System.out.println("LINE_NUM: " + line_Num);
             }
-            System.out.println("boy_count" + boy_count);
-            System.out.println("girl_count" + girl_count);
-            System.out.println("boy_sum" + boy_sum);
-            System.out.println("girl_sum" + girl_sum);
+
         }
+        System.out.println("Your file contains: " + boy_count + " boys and " + girl_count + " girls.");
+        System.out.println("The different between boys and girls sums:  " + Math.abs(boy_sum - girl_sum));
     }
 }
