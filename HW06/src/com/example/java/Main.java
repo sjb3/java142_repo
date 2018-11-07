9b89f705f0eae65b31acd11a2c9a262988e93e99
Sung J Byun <sungjbyun@Sungs-MacBook-Pro-2.local>
2018-11-07 09:25:16 -0800

    working on HW07

--- HW06/src/com/example/java/Main.java
+++ HW06/src/com/example/java/Main.java
@@ -1,30 +1,24 @@
 package com.example.java;
 import java.util.*;
-
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("printFactors #9");
-        System.out.println("Enter a number that you wanna print out the factors! ");
-        Scanner console = new Scanner(System.in);
-        int input = console.nextInt();
-        System.out.println(printFactors(input));
+        int [] seq1 = {1,6,2,1,4,1,2,1,8};
+        int [] seq2 = {1,2,1};
+
+        System.out.println("#14 contains: ");
+        System.out.println(contains(seq1, seq2));
     }
 
-    public static String printFactors(int num) {
-        if(num<=0) {
-            System.out.println("Number shouldn\'t be negative or 0");
+    public static boolean contains(int[] a1, int[] a2) {
+        boolean answer = false;
+//         if(Arrays.asList(a1).containsAll(Arrays.asList(a2))) {
+//             answer = true;
+//         }
+        if(Arrays.asList(a1).contains(Arrays.asList(a2))) {
+            answer = true;
         }
-
-        String results = "";
-
-        int i = 1;
-        while(i < num) {
-            if(num%i ==0) {
-                results += i + " and ";
-            }
-            i++;
-        }
-        return results + num;
+        return answer;
     }
 }
+
